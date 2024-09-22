e49a68ad65c2ac78e9a9c791e47f7ad5a83e2a12
austinh-io <github@msg.austinh.io>
2024-09-21 19:38:20 -0700

    Header category icon now appears and changes icon based on category clicked

--- src/main.js
+++ src/main.js
@@ -2,10 +2,41 @@ import { initLightToggle } from './lightswitch.js';
 import { quizzes } from './quizManager.js';
 import { initQuizChoiceButtons } from './screenTransitionManager.js';
 
-console.log(quizzes);
+const appHeaderQuizCategory = document.querySelector(
+	'.app-header-quiz-category'
+);
+const currentQuizImage = document.getElementById('current-quiz-label-image');
 
-console.log(quizzes['HTML'].questions[2].answer);
-// console.log(quizzes['HTML'].questions[1]);
+const btnHTMLQuiz = document.getElementById('btn-quiz-choice-html');
+const btnCSSQuiz = document.getElementById('btn-quiz-choice-css');
+const btnJSQuiz = document.getElementById('btn-quiz-choice-js');
+const btnAccessibilityQuiz = document.getElementById(
+	'btn-quiz-choice-accessibility'
+);
+
+function setCurrentQuizImage(value) {
+	appHeaderQuizCategory.classList.remove('hidden');
+	currentQuizImage.src = `${value}`;
+}
+
+function initQuizButtons() {
+	btnHTMLQuiz.addEventListener('click', () => {
+		setCurrentQuizImage(quizzes['HTML'].icon);
+	});
+
+	btnCSSQuiz.addEventListener('click', () => {
+		setCurrentQuizImage(quizzes['CSS'].icon);
+	});
+
+	btnJSQuiz.addEventListener('click', () => {
+		setCurrentQuizImage(quizzes['JavaScript'].icon);
+	});
+
+	btnAccessibilityQuiz.addEventListener('click', () => {
+		setCurrentQuizImage(quizzes['Accessibility'].icon);
+	});
+}
 
 initLightToggle();
 initQuizChoiceButtons();
+initQuizButtons();
